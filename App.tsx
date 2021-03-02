13a91499f3bf79588c54b61278f2476f714615da
Julian Khatibi <ajkhatibi@gmail.com>
2021-03-02 08:40:56 -0800

    added autocomplete

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, SafeAreaView } from "react-native";
-import { SearchBar } from "react-native-elements";
+import Autocomplete from "react-native-autocomplete-input";
+import data from "./breeds.json";
+
+const findDogs = (query) => {
+  if (query === "") {
+    return [];
+  }
+
+  const regex = new RegExp(`${query.trim()}`, "i");
+  return data.dogs.filter((item) => item.search(regex) >= 0);
+};
 
 export default function App() {
+  const [query, setQuery] = useState("");
+  const dogList = findDogs(query);
+  const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
   return (
     <SafeAreaView style={styles.container}>
-      <SearchBar placeholder="Type Here..." value={"search"} />
+      <Autocomplete
+        placeholder="Start typing..."
+        data={dogList.length === 1 && comp(query, dogList[0]) ? [] : dogList}
+        defaultValue={query}
+        onChangeText={(text) => setQuery(text)}
+        renderItem={({ item, i }) => <Text key={i}>{item}</Text>}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
