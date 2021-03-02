8129a1b143fa2d61c9cd26566ba0f096db918156
Julian Khatibi <ajkhatibi@gmail.com>
2021-03-01 17:50:00 -0800

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { SearchBar } from "react-native-elements";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <SearchBar placeholder="Type Here..." value={"search"} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
